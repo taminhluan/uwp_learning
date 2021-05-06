aff16a788b277c84a19355dc436c036b6ba44825
taminhluan <ngoalongtb001@gmail.com>
2021-05-06 22:46:44 +0900

    init project, demo func push notification

---
+++ Learning/MainPage.xaml.cpp
@@ -0,0 +1,42 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace Learning;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace Microsoft::Toolkit::Uwp::Notifications;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
+
+
+void Learning::MainPage::inputButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	greetingOutput->Text = "Hello " + nameInput->Text;
+
+    // Construct the content and show the toast!
+    (ref new ToastContentBuilder())
+        ->AddArgument("action", "viewConversation")
+        ->AddArgument("conversationId", 9813)
+        ->AddText("Andrew sent you a picture")
+        ->AddText("Check this out, The Enchantments in Washington!")
+        ->Show();
+}
